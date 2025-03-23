a39a88609559a99a7f5b31a9c040205910842cfd
Javier Martinez <hoogmin@gmail.com>
2025-03-23 16:19:02 -0400

    Fundamental layout and styles.

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,44 @@
-import Image from "next/image";
+"use client"
+
+import { useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // Refs for both code input and lock type select so we don't trigger unneeded re-renders.
+  const codeRef = useRef(null);
+  const lockTypeRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+    <main className="min-h-screen min-w-screen flex flex-col justify-items-center ribbon">
+      <div className="flex flex-col text-center mx-auto my-9">
+        <h1 className="text-2xl">NX500</h1>
+        <h2 className="text-base">safe-locksmith.com</h2>
+        <p className="text-base">v1.0</p>
+      </div>
+      <div className="flex flex-col space-x-2 text-center mx-auto my-2">
+        <p className="text-base" id="keypadStatus">
+          <span className="status-dot initializing mr-1.5"></span>
+          Keypad initializing
+        </p>
+        <p className="text-base" id="powerInfo">
+          <span className="status-dot ok mr-1.5"></span>
+          Battery Voltage 9.0V Consumption 10.0 mW
+        </p>
+      </div>
+      <div className="flex flex-col space-y-1.5 text-center mx-auto my-2">
+        <label htmlFor="lockType">Select Lock Type</label>
+        <select name="lockType" id="lockType" ref={lockTypeRef}>
+          <option value="ESL5">ESL5</option>
+          <option value="ESL10/20">ESL10/20</option>
+          <option value="S&G">S&G</option>
+          <option value="LG">LG</option>
+          <option value="NL">NL</option>
+          <option value="SecuRam">SecuRam</option>
+        </select>
+        <label htmlFor="code">Code</label>
+        <input name="code" id="code" type="text" placeholder="e.g. 12345" ref={codeRef}/>
+        <button className="nxbutton mt-4" id="btnSendCode">Send Code</button>
+        <button className="nxbutton" id="btnCheckForLockout">Check For Lockout</button>
+      </div>
+    </main>
   );
 }
